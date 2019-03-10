b2f4cb00bc584813f6ba026c920bfafb168b6c16
Will <will.js@protonmail.com>
2019-03-10 09:54:44 -0500

    Docstrings

--- cdds/CDDSMainWindow.py
+++ cdds/CDDSMainWindow.py
@@ -1,7 +1,9 @@
+"""This module creates the main window"""
+
 from PyQt5.QtWidgets import QMainWindow
 
 class CDDS (QMainWindow):
-    """Create the main window"""
+    """This class initializes the main window"""
 
     def __init__(self, parent=None):
         super(CDDS, self).__init__(parent)
